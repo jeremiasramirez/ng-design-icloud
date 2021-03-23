848a3b533c8395b7f085d52a6ceebd380ef3aeb4
Jeremias ramirez <jereramirez499@gmail.com>
2021-03-23 19:52:54 -0400

    agregado de pagina de setting y apps de iclouds

---
+++ src/app/pages/apps/apps.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+  selector: 'apps-page-root',
+  templateUrl: './apps.page.html',
+  styleUrls: ['./apps.page.css']
+})
+export class AppsPage {
+ 
+ 
+}
---
+++ src/app/pages/setting/setting.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+  selector: 'app-setting-root',
+  templateUrl: './setting.page.html',
+  styleUrls: ['./setting.page.css']
+})
+export class SettingPage {
+ 
+ 
+}
